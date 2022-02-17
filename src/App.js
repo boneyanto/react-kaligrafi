48be0496be32e91ac6d5e209cb304a0a531a6175
boneyanto <nurdiyanto7@gmail.com>
2022-02-17 09:53:09 +0900

    2 files modified, 2 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, {useState} from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
+import './hidayatullah.ttf';
 
 export default function App() {
-  const [huruf,setHuruf] = useState("");
+  const [huruf, setHuruf] = useState('');
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <input type="text" name="huruf" onChange={(e)=>setHuruf(e.target.value)} />
-      <h2 >{huruf}</h2><br/>
-      <h2 StyleSheet="fontFamily: 'Calibri';">{huruf}</h2><br/>
-      <h2 StyleSheet="fontFamily: 'Sans';">{huruf}</h2><br/>
+      <input
+        type="text"
+        name="huruf"
+        onChange={(e) => setHuruf(e.target.value)}
+      />
+      <h2>{huruf}</h2>
+      <br />
+      <h2 className="font-face-gm">{huruf}</h2>
+      <br />
+      <h2 StyleSheet="fontFamily: 'Sans';">{huruf}</h2>
+      <br />
     </div>
   );
-  
 }
